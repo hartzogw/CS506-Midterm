3d16812b514bb38f474a25972e9670fa5157c76b
Colton <cdstarkey1s@semo.edu>
2024-03-15 08:10:36 -0500

    Added functions to find a check the status of a server and find an operational server

--- CS506_Midterm.py
+++ CS506_Midterm.py
@@ -1,6 +1,6 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import threading
-
+import socket
 
 #Create server instances
 def run_server(port):
@@ -8,6 +8,22 @@ def run_server(port):
   httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
   print(f"Server instance started at port {port}\n")
   httpd.serve_forever()
+
+def check_server(address):
+  try:
+    response = requests.get(address + '/health', timeout=1)
+    if response.status_code == 200:
+      return True
+    else:
+      return False
+  except:
+    return False
+
+#find a server
+def find_server():
+  if check_server:
+    return check_server()
+  return None
 
 # Start servers using threading
 threads = []
