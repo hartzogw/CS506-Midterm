e03ab2aa1bb3f0822e72a913cec98bf46a1d40af
Maxemus2000 <134463121+Maxemus2000@users.noreply.github.com>
2024-03-17 22:18:08 -0500

    Multithread Client
    Update Markell's script to allow multiple thread chat

--- client.py
+++ client.py
@@ -1,37 +1,66 @@
-import socket,time
+import socket
+import threading 
 
-
-
-server_port = 8000
-
-def run_client():
-    time.sleep(5)
-    # create a socket object
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    server_ip = "localhost"  # replace with the server's IP address
-
-    # establish connection with server
-    
-    client.connect((server_ip, server_port))
-    
-
+def receive(client, name):
     while True:
-        # input message and send it to the server
-
-        msg = input("Enter a new port number\n ")
-        client.send(msg.encode("utf-8")[:1024])
-
-        # receive message from the server
-        response = client.recv(1024)
-        response = response.decode("utf-8")
-
-        # if server sent us "closed" in the payload, we break out of the loop and close our socket
-        if response.lower() == "closed":
+        try:
+            # Message recieves from server
+            msg = client.recv(1024).decode("utf-8")
+            if msg == 'Nickname':
+                client.send(name.encode("utf-8"))            
+            else:
+                print(msg)
+        except:
+            print("Server shuts down!")
+            client.close()
             break
 
-    # close client socket (connection to the server)
-    client.close()
-    print("Connection to server closed")
+def write(client, name):
+    while True:
+        # Get user input
+        msg = input("")
+        
+        # Keyword to leave the chat
+        if msg.lower() == 'closed':
+            print("\nGoodbye")
+            client.close()
+            break
+        
+        # Send message to server
+        msg =f'{name}: {msg}'        
+        client.send(msg.encode("utf-8"))
+           
+def run_client():
+    # Let user select the server
+    while True:
+        print("\nAvailable Port Numbers: 8080, 8081, 8082")
+        print("Enter Port Number: ")
+        msg = input("")
+        
+        # Check user input
+        if msg == '8080' or msg == '8081' or msg =='8082':
+            break
+        else:
+            print("\nInvalid Port!")
+    
+    # Server hostname or IP address
+    server_ip = "localhost"  
+    server_port = int(msg)
+    name = input("Enter a nickname: ")
+    
+    # Create a socket object
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Establish connection with server
+    client.connect((server_ip, server_port))
+    
+    # Start thread for recieving message    
+    receive_thread = threading.Thread(target=receive, args=(client, name))
+    receive_thread.start()
+    
+    # Start thread to send message
+    write_thread = threading.Thread(target=write, args=(client, name))
+    write_thread.start()
 
-run_client()
+if __name__ == "__main__":
+    run_client()
